63c2f22651e6c56c944a74f132d6ca326e1d286f
Jose Amador <joseamadorzn@gmail.com>
2018-11-12 14:45:06 -0500

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,72 @@
-# article-meeseeks
-Article gatherer chatbot for Telegram
+# Article Meeseeks
+
+> Article gatherer chatbot for Telegram.
+
+
+Chatbot for Telegram platform that pulls every link it receives and saves it in a Google Drive spreadsheet to build a knowledge database and share it with the rest of your team.
+
+The purpose of this chatbot is to take advantage of the already existing information sharing platforms in your team by gathering all the juicy articles and creating a knowledge repository from where you can choose to share to social media, internally or just save them for later.
+
+![Look at me!](https://s3.amazonaws.com/helpbucket.tektonlabs.com/uploads/960684c3-4a3c-432b-b25a-5d7f4405283b/c8758ba4-7f27-4a4a-8950-1084ee158cd5/meese.jpg)
+
+## Installation / Development setup
+
+Run this in a terminal:
+
+```sh
+git clone https://github.com/joamadorrr/article-meeseeks.git
+
+cd article-meeseeks
+
+cp .env.example .env
+
+```
+
+We just created a file with all the environment variables you will require:
+
+* BOT_TOKEN: Once you've created your bot, talk to the BotFather and ask him for your bot's token
+* SHEETS_SCOPE: Unles you want to do something weird, leave the default
+* SPREADSHEET_ID: To get the id for your spreadsheet read this: https://developers.google.com/sheets/api/guides/concepts#spreadsheet_id
+* RANGE_NAME: Read the A1 notation part in the link above
+
+If you are setting up a development environment I highly recommend you use pipenv. Otherwise, you can install the requirements from the requirements.txt file.
+
+### Pipenv
+
+Here we create an environment for the chatbot in python 3 and copy the last version of the development environment from the pipfile.lock
+
+```sh
+pipenv shell --three
+
+pipenv install --ignore-pipfile
+
+```
+
+## Usage example
+
+Hmm basically just talk to it and send him links to cool articles you wanna share with your team. A really cool feature of Telegram bots is that you can make them join group conversations, so the Article Meeseeks can infiltrate different conversations and gather articles from those too.
+
+## Release History
+
+* 0.1.0
+   * The first proper release
+   * Environment variables support added
+* 0.0.1
+   * It works but a lot of things need to be enhanced before "official launch"
+
+
+## Meta
+
+Jo Amador – [@joamadorrr](https://twitter.com/joamadorrr)
+
+Distributed under the MIT license. See `LICENSE` for more information.
+
+[https://github.com/joamadorrr](https://github.com/joamadorrr/)
+
+## Contributing
+
+1. Fork it (<https://github.com/joamadorrr/article-meeseeks/fork>)
+2. Create your feature branch (`git checkout -b feature/fooBar`)
+3. Commit your changes (`git commit -am 'Add some fooBar'`)
+4. Push to the branch (`git push origin feature/fooBar`)
+5. Create a new Pull Request
---
+++ meeseeks.py
@@ -0,0 +1,90 @@
+import time, datetime, os
+from os.path import join, dirname
+from dotenv import load_dotenv
+from googleapiclient.discovery import build
+from httplib2 import Http
+from oauth2client import file, client, tools
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+def start(bot, update):
+    update.message.reply_text("I'm Librarian Meeseeks! Share articles with me and I will collect and share them with the rest of the team!")
+
+def help(bot, update):
+    update.message.reply_text("Send me a link you consider interesting and I will take care of the rest.")
+
+def collect(bot, update):
+    print('new message:')
+    text = update.message.text
+    print(text)
+    first_name = update.message.from_user.first_name
+    last_name = update.message.from_user.last_name
+
+    if first_name is None:
+        full_name = 'Unknown'
+    elif last_name is None:
+        full_name = first_name
+    else:
+        full_name = first_name + ' ' + last_name
+
+    key = "https://"
+
+    if key in text:
+        pos = text.find(key)
+        ts = time.time()
+        st = datetime.datetime.fromtimestamp(ts).strftime('%Y/%m/%d %H:%M:%S')
+
+        values = [
+            [
+                st,
+                full_name,
+                "Chatbot can't read titles yet...",
+                text[pos:].split(' ')[0]
+            ],
+        ]
+
+        body = {
+            'values': values
+        }
+
+        print(body)
+
+        result = service.spreadsheets().values().append(
+            spreadsheetId=os.getenv('SPREADSHEET_ID'), range=os.getenv('RANGE_NAME'),
+            valueInputOption='USER_ENTERED', body=body).execute()
+
+        print('{0} cells appended.'.format(result \
+                                           .get('updates') \
+                                           .get('updatedCells')))
+
+        bot.send_message(chat_id=update.message.chat_id, text="Saving that for laterrr!")
+
+def unknown(bot, update):
+    update.message.reply_text("Existence is pain!")
+
+def main():
+    dotenv_path = join(dirname(__file__), '.env')
+    load_dotenv(dotenv_path)
+
+    store = file.Storage('token.json')
+    creds = store.get()
+
+    if not creds or creds.invalid:
+        flow = client.flow_from_clientsecrets('credentials.json', os.getenv('SHEETS_SCOPE'))
+        creds = tools.run_flow(flow, store)
+
+    global service
+    service = build('sheets', 'v4', http=creds.authorize(Http()))
+
+    updater = Updater(token=os.getenv('BOT_TOKEN'))
+    dp = updater.dispatcher
+
+    dp.add_handler(CommandHandler('start', start))
+    dp.add_handler(CommandHandler('help', help))
+    dp.add_handler(MessageHandler(Filters.text, collect))
+    dp.add_handler(MessageHandler(Filters.command, unknown))
+
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
